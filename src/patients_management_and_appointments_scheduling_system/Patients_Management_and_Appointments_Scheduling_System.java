e2a1519c84f818657f65f2836749a294c655242c
AntoineMHB <antoinemuhumbania@gmail.com>
2023-11-29 12:29:53 +0200

    Initial commit

---
+++ src/patients_management_and_appointments_scheduling_system/Patients_Management_and_Appointments_Scheduling_System.java
@@ -0,0 +1,22 @@
+
+package patients_management_and_appointments_scheduling_system;
+
+import com.forms.Login;
+import com.forms.Login;
+import com.forms.SignUp;
+import com.forms.SignUp;
+import javax.swing.SwingUtilities;
+
+public class Patients_Management_and_Appointments_Scheduling_System {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login loginPage = new Login();
+        loginPage.show();
+        
+//       SwingUtilities.invokeLater(() -> {
+//        new SignUp();
+//                });
+    }
+    
+}
